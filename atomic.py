0d15f1aaf94f717780edc2f77d8be251de035dfd
0xgrowth <sapientiakonzept@gmail.com>
2024-01-17 02:35:16 +0100

    Second commit

--- atomic.py
+++ atomic.py
@@ -30,11 +30,7 @@ WHITE = (255, 255, 255)
 def screen_color():
     screen.fill(BLACK)
 
-# --------------------------
-
-# colors = [red_display,blue_display,green_display,cyan_display,gray_display,black_display,yellow_display,magenta_display]
-
-#ball one
+#First Set of particles
 ball = []
 a_pos = []
 b_pos = []
@@ -56,8 +52,33 @@ for i in range(num_of_ball) :
     apos_change.append(2)
     bpos_change.append(2)
 
-def ballImg(a,b,i) :
+# Second set of particles
+ball_one = []
+c_pos = []
+d_pos = []
+cpos_change = []
+dpos_change = []
+num_of_ball_one = 10000
+
+for i in range(num_of_ball_one) :
+    bel = pygame.image.load('ball.png')
+    small_bell = pygame.transform.scale(bel, (3,3))
+    ball_one.append(small_bell)
+    # horizontal(x) position of snowballs
+    c_pos.append(random.randint(10, 895))
+
+    # vertical(x) position of snowballs
+    d_pos.append(random.randint(5, 698))
+
+    # constants for ball speed 
+    cpos_change.append(-2)
+    dpos_change.append(2)
+
+def ballImg(a,b,i):
     screen.blit(ball[i], (a,b))
+
+def ballImg_one(a,b,i):
+    screen.blit(ball_one[i], (a,b))
 
 # fonts = pygame.font.get_fonts()
 # for f in fonts:
@@ -86,11 +107,25 @@ while True:
             apos_change[i] = -2
         elif a_pos[i] <= 10 :  
             apos_change[i] = +2
-
     # ---  
- 
+    for i in range(num_of_ball_one) :
+        c_pos[i] += cpos_change[i]
+        d_pos[i] += dpos_change[i]
+
+    # --- bouncing effect with  screen boundaries
+        if c_pos[i] <= 5 :
+            cpos_change[i] = +2
+        elif c_pos[i] >= 895 :
+            cpos_change[i] = -2
+
+        if d_pos[i] >= 698 :
+            dpos_change[i] = -2
+        elif d_pos[i] <= 5 :  
+            dpos_change[i] = +2
+
+    
     # --- calling the functions for atomic motion
-        ballImg(a_pos[i],b_pos[i], i)
+        #ballImg(a_pos[i],b_pos[i], i)
         ballImg_one(c_pos[i],d_pos[i], i)
     # ---
 
@@ -100,4 +135,4 @@ while True:
             pygame.quit()
     # ---
 
-    pygame.display.update()        
+    pygame.display.flip()        
